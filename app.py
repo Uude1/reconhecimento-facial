0a7b9147f6a0316d857618e091ae20c11ad51817
Diego Priess <diegopriess.dev@gmail.com>
2023-11-11 16:57:15 -0300

    Consulta imagens do banco

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import base64
 import cv2
 import dlib
-import os
 import numpy as np
+import mysql.connector
 
-cap = cv2.VideoCapture(2) 
+img_folder_path = "img/"
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="ifmatch"
+)
+
+cursor = conexao.cursor()
+query = "SELECT * FROM user"
+cursor.execute(query)
+results = cursor.fetchall()
+
+cap = cv2.VideoCapture(0)
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")  
@@ -39,13 +54,15 @@ while True:
         found_similar_face = False
 
         if webcam_face_landmarks is not None:
-            for img_filename in os.listdir(img_folder_path):
-                img_path = os.path.join(img_folder_path, img_filename)
+            for user in results:
 
-                img = cv2.imread(img_path)
-                gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                file_name = str(user[0]) + ".jpg"
+                image_data = base64.b64decode(user[4])
+                nparr = np.frombuffer(image_data, np.uint8)
+                image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+                gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-                frame_resized = cv2.resize(frame, (int(frame.shape[1] * img.shape[0] / frame.shape[0]), img.shape[0]))
+                frame_resized = cv2.resize(frame, (int(frame.shape[1] * image.shape[0] / frame.shape[0]), image.shape[0]))
 
                 img_faces = detector(gray_img, 0)
 
@@ -55,19 +72,19 @@ while True:
                     img_face_landmarks = landmarks_img.parts()
 
                     for point in img_face_landmarks:
-                        x, y = point.x * original_width / img.shape[1], point.y * original_height / img.shape[0]
+                        x, y = point.x * original_width / image.shape[1], point.y * original_height / image.shape[0]
                         point.x, point.y = int(x), int(y)
 
                     distance = np.mean([np.linalg.norm(np.array((x1.x - x2.x, x1.y - x2.y))) for x1, x2 in zip(webcam_face_landmarks, img_face_landmarks)])
 
-                    threshold = 20  
+                    threshold = 20
                     if distance < threshold:
                         found_similar_face = True
-                        print(f"Rosto na webcam é parecido com {img_filename} (Distância: {distance})")
+                        print(f"Rosto na webcam é parecido com {file_name} (Distância: {distance})")
 
-                        combined_image = np.concatenate((frame_resized, img), axis=1)
+                        combined_image = np.concatenate((frame_resized, image), axis=1)
                         cv2.imshow('Webcam vs. Image', combined_image)
-                        cv2.waitKey(0) 
+                        cv2.waitKey(0)
 
         if not found_similar_face:
             cv2.imshow('Webcam Face Detection', frame)
